4de96f981bea025b1ba7a173ebce9b439f9cc40e
yanashige <yanagi.shigeyuki@gmail.com>
2015-05-02 13:14:01 +0900

    Create outofmemory.java

---
+++ outofmemory.java
@@ -0,0 +1,19 @@
+class outofmemory {
+ public static void main(String args[])throws Exception{
+    for (int i = 0 ; i < 100 ;i++) {
+Thread thread = new Thread (new MyOperation());
+thread.start();
+
+while(thread.isAlive()){
+thread.sleep(500);
+   }
+  }
+ }
+}
+
+class MyOperation extends Thread{
+protected double[] huge_buffer = null;
+public void run(){
+huge_buffer = new double[100000];
+ }
+}
